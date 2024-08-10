0f89c4beb1686af72f31092949f030c5077cceac
aerzhanm <erjanych1@gmail.com>
2024-08-10 13:01:51 +0600

    ytro

--- hero.py
+++ hero.py
@@ -48,7 +48,7 @@ class Hulk(SuperHero):
 
     def duble_health(self):
         self.health_points = self.health_points ** 2
-        self.fly
+        self.fly = True
         return self.health_points, self.fly
 
     def __str__(self):
@@ -57,24 +57,60 @@ class Hulk(SuperHero):
                 f' мое здоровье {self.health_points}\n'
                 f'коронная фраза {self.catchphrase}')
 
-    def fly(self):
+    def flyy(self):
         return f'я летаю но только хлопками'
 
     def phrase(self):
         return 'True in the True_phrase'
 
 
-hulk = Hulk('Робеерот Брюс Беннер', 'Халк'
+hulk = Hulk('Робет Брюс Беннер', 'Халк'
             , 'летать, бить, гнев', 6000, 'Не зли меня а то убью', 2000)
 print(hulk)
+print(hulk.phrase())
+print(hulk.flyy())
+print(hulk.duble_health())
 
+class Betmen(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, super_hit, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.super_hit = super_hit
+        self.fly = fly
 
+    def duble_health(self):
+        self.health_points = self.health_points ** 2
+        self.fly = True
+        return self.health_points, self.fly
+
+    def __str__(self):
+        return (f'меня зовут {self.name}, но все меня знают как {self.nickname}\n'
+                f'мои суперспособности это {self.superpower}\n'
+                f' мое здоровье {self.health_points}\n'
+                f'коронная фраза {self.catchphrase}')
+
+    def flyy(self):
+        return f'я летаю но только хлопками'
+
+    def phrase(self):
+        return 'True in the True_phrase'
+
+bruse = Betmen('Брюс Уэйн', 'Бэтмен', 'тратить деньги и зашищать', 6000, 'Я работаю только один', None, '500 метров')
+
+print(bruse)
 class Villain(Hulk):
     people = 'monster'
 
-    def __init__(self, name, nickname, superpower, health_points, catchphrase, super_hit, fly=False, damage):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, super_hit, damage, fly=False):
         super().__init__(name, nickname, superpower, health_points, catchphrase, super_hit, fly)
         self.damage = damage
+
+    def __str__(self):
+        return (f'меня зовут {self.name}, но все меня знают как {self.nickname}\n'
+                f'мои суперспособности это {self.superpower}\n'
+                f' мое здоровье {self.health_points}\n'
+                f'коронная фраза {self.catchphrase}\n'
+                f'урон: {self.damage}')
+
 
     def gen_x(self):
         pass
@@ -82,3 +118,10 @@ class Villain(Hulk):
     def crit(self):
         self.damage = self.damage ** 2
         return self.damage
+
+kloun = Villain('гена', 'клоун', 'обманывать и убивать',
+             3000, 'xa-xa-xa-xa', None, 4000)
+
+print(kloun.crit())
+print(kloun)
+print(kloun.duble_health())
